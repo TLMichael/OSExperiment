6230db41017afa6046b99e8a686e5b13fff774f1
TLMichael <756535645@qq.com>
2018-06-18 22:25:22 +0800

    这次更新的test.c实现了循环管道

--- program_task/multi_process/test.c
+++ program_task/multi_process/test.c
@@ -5,6 +5,7 @@
 #include <fcntl.h>
 
 int fd[2];
+int fd_tmp[2];
 int flag;
 
 void f1()
@@ -43,6 +44,8 @@ int pipe_sys(const char *cmdstring)
     {  
 		if(flag == 0)
 		{
+            printf("[flag] %d\t[code] %s\n", flag, cmdstring);
+
 			dup2(fd[1], 1);
 			close(fd[0]);
 			close(fd[1]);
@@ -51,13 +54,29 @@ int pipe_sys(const char *cmdstring)
 		}
 		else if(flag == 1)
 		{
+            printf("[flag] %d\t[code] %s\n", flag, cmdstring);
+
 			dup2(fd[0], 0);
 			close(fd[0]);
 			close(fd[1]);
 			execl("/bin/sh", "sh", "-c", cmdstring, (char *)0);  
         	exit(127); 
 		}
-         
+        else if(flag == 2)
+        {
+            printf("[flag] %d\t[code] %s\n", flag, cmdstring);
+
+            dup2(fd[0], 0);
+            close(fd[0]);
+            close(fd[1]);
+            // 输出进入临时管道
+            dup2(fd_tmp[1], 1);
+            close(fd_tmp[0]);
+            close(fd_tmp[1]);
+            execl("/bin/sh", "sh", "-c", cmdstring, (char *)0);  
+        	exit(127); 
+        }
+        wait(NULL);
     }  
     return 0;  
 }
@@ -65,11 +84,31 @@ int pipe_sys(const char *cmdstring)
 int main()
 {
     pipe(fd);
+    pipe(fd_tmp);
 
 	flag = 0;
-	pipe_sys("cat /etc/passwd");
-	flag = 1;
-	pipe_sys("wc -l");
+	pipe_sys("cat < input.txt");
+
+	flag = 2;
+	pipe_sys("sort");
+    // 将临时管道变成正式的管道
+    dup2(fd_tmp[0], fd[0]);
+    dup2(fd_tmp[1], fd[1]);
+    close(fd_tmp[0]);
+    close(fd_tmp[1]);
+    pipe(fd_tmp);
+ 
+    flag = 2;
+    pipe_sys("uniq");
+    dup2(fd_tmp[0], fd[0]);
+    dup2(fd_tmp[1], fd[1]);
+    close(fd_tmp[0]);
+    close(fd_tmp[1]);
+    pipe(fd_tmp);
+
+    flag = 1;
+    pipe_sys("cat >output.txt");
+
 
     // flag = 0;
     // f1();
